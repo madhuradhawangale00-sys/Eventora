d0bbe5357adc348939b2bc0e8b2b6d3cd6b4c637
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-07-22 23:40:36 +0530

    Intalled dependencies

---
+++ server/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const mongoose = require('mongoose');
+
+dotenv.config();
+
+const app = express();
+
+mongoose.connect(process.env.MONGODB_URI)
+.then(() => {
+    console.log('Connected to MongoDB');
+    
+})
+.catch((error) => {
+    console.error("Error connecting to MongoDB",error );
+    
+})
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () =>{
+    console.log(`Server is running on port ${PORT}`);
+    
+
+})
