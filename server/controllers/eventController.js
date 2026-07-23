982d8b810c9c9bc71c896441228a11079859dd0c
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-07-23 23:54:04 +0530

    created eventController

---
+++ server/controllers/eventController.js
@@ -0,0 +1,73 @@
+const Event = require('../models/Event');
+
+exports.getAllEvents = async (req, res) => {
+    try{
+
+        const filters = {};
+        if(req.query.category) {
+            filters.category = req.query.category;
+        }
+        if(req.query.ticketPrice){
+            filters.ticketPrice = req.query.ticketPrice;
+        }
+        const events = await Event.find(filters);
+        res.json(events);
+    } catch(error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+exports.getEventById = async (req,res) => {
+    try{
+        const event = await Event.findById(req.params.id);
+        if(!event){
+            return res.status(404).json({error: 'Event not found'});
+        }
+        res.json(event);
+    }catch(error) {
+        res.status(500).json({error: error.message});
+    }
+};
+
+exports.createEvent = async (req, res) => {
+    try {
+        const { title, description, date, location, category, totalSeats, ticketPrice, image } = req.body;
+        const event = await Event.create({
+            title,
+            description,
+            date,
+            location,
+            category,
+            totalSeats,
+            availableSeats: totalSeats,
+            ticketPrice: ticketPrice || 0,
+            image: image || '',
+            createdBy: req.user.id
+        });
+        res.status(201).json(event);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+exports.updateEvent = async (req, res) => {
+    try {
+        const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        if (!event) return res.status(404).json({ message: 'Event not found' });
+        res.json(event);
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+exports.deleteEvent = async (req, res) => {
+    try {
+        const event = await Event.findByIdAndDelete(req.params.id);
+        if (!event) return res.status(404).json({ message: 'Event not found' });
+        res.json({ message: 'Event deleted successfully' });
+    } catch (error) {
+        res.status(500).json({ message: 'Server Error', error: error.message });
+    }
+};
+
+
