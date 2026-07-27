18fb9044c1c40c3ce37e98cc79ce1dc415933252
madhuradhawangale00-sys <madhuradhawangale00@gmail.com>
2026-07-27 23:52:45 +0530

    created src files

---
+++ client/src/utils/axios.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+import { configs } from "eslint-plugin-react-hooks";
+
+const api = axios.create({
+    baseURL: 'http://localhost:5000/api',
+    headers: {
+        "Content-Type":'application/json',
+    },
+});
+
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem('token');
+    if(token) {
+        config.headers.Authorization = `Bearer ${token}`;
+    }
+   return config; 
+});
+
+export default api;
